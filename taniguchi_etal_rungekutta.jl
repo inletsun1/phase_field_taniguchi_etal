5db307e782a03f4f6de605e82353dd0ece15fe38
Yoichi Irie <yoichi.m.i430@gmail.com>
2018-11-25 18:24:18 +0900

    version 1.0

--- taniguchi_etal_rungekutta.jl
+++ taniguchi_etal_rungekutta.jl
@@ -2,10 +2,10 @@ using PyPlot
 using JLD
 
 
-const XSIZE = 200
-const YSIZE = 200
-const TSIZE = 1000 
-const dx = 0.1
+const XSIZE = 100
+const YSIZE = 100
+const TSIZE = 4000 
+const dx = 0.2
 const dy = dx
 const Du = 0.05
 const Dv = 0.20
@@ -18,6 +18,7 @@ const eta = 1.0
 const M = 0.5
 const A0 = 5.0^2*pi
 const R = 5.0
+
 
 function main()
     #parameters
@@ -49,10 +50,11 @@ function main()
     function fp(U, V, phi)
         return -eta./(tau.*eps.^2).*36.0.*phi.*(1.0.-phi).*(1.0.-2.0.*phi)
     end
+
     #initialization
-    phi = zeros(Float64, YSIZE, XSIZE, TSIZE);
-    U = zeros(Float64, YSIZE, XSIZE, TSIZE);
-    V = zeros(Float64, YSIZE, XSIZE, TSIZE);
+    phi = zeros(Float64, YSIZE, XSIZE, TSIZE)
+    U = zeros(Float64, YSIZE, XSIZE, TSIZE).+1.0/gamma
+    V = zeros(Float64, YSIZE, XSIZE, TSIZE)
     phase_range = 0.01
     delta = dx*5
     k = log((1.0 - phase_range)/phase_range)/delta
@@ -61,8 +63,10 @@ function main()
     r0 = R + delta/2.0
     rmesh = [k*(sqrt((x*dx-x0)^2 + (y*dy-y0)^2)-r0) for y in 0:YSIZE-1, x in 0:XSIZE-1]
     phi[:,:,1] = (1.0 .- tanh.(rmesh))./2.0
+    V[30:35, 30:35, 1] .= 1.5
     #V[:,:,1] = (1.0 .- tanh.(rmesh))./2.0
-    #U[:,:,1] = 6.0.*(1.0 .- tanh.(rmesh))./2.0
+    #U[:,:,1] = (1.0 .- tanh.(rmesh))./(2.0*gamma)
+    #=
     for x in 1:XSIZE
         for y in 1:YSIZE
             if ((x-1)*dx-x0)^2 + ((y-1)*dy-y0)^2<R-1.0
@@ -70,6 +74,7 @@ function main()
             end
         end
     end
+    =#
 
 
 
@@ -106,17 +111,24 @@ function main()
     println("start calculation")
     #time development
     for t in 2:TSIZE
+
+        k1u = fu(U[:,:,t-1], V[:,:,t-1], phi[:,:,t-1])
+        k1v = fv(U[:,:,t-1], V[:,:,t-1], phi[:,:,t-1])
+        k1p = fp(U[:,:,t-1], V[:,:,t-1], phi[:,:,t-1])
+        k2u = fu(U[:,:,t-1].+k1u.*dt, V[:,:,t-1].+k1v.*dt, phi[:,:,t-1].+k1p.*dt)
+        k2v = fv(U[:,:,t-1].+k1u.*dt, V[:,:,t-1].+k1v.*dt, phi[:,:,t-1].+k1p.*dt)
+        k2p = fp(U[:,:,t-1].+k1u.*dt, V[:,:,t-1].+k1v.*dt, phi[:,:,t-1].+k1p.*dt)
+
         A = sum(phi[:,:,t-1].*dx.*dy)
 
         abs_nabra_phi = sqrt.(((1.0 ./(2.0.*dx)) .* phi[:,:,t-1] *Bx).^2 .+ ((1.0 ./(2.0.*dy)) .* By *phi[:,:,t-1]).^2)
 
-        del_phi = (eta.*((1.0 ./dy) .^2.0 .* Ay * phi[:,:,t-1] .+ (1.0 ./dx) .^2.0 .* phi[:,:,t-1] * Ax) .-M.*(A .- A0).*abs_nabra_phi .+ (a.*V[:,:,t-1] .- b.*U[:,:,t-1]).*abs_nabra_phi)./tau .+ fp(U[:,:,t-1], V[:,:,t-1], phi[:,:,t-1])
-
+        del_phi = (eta.*((1.0 ./dy) .^2.0 .* Ay * phi[:,:,t-1] .+ (1.0 ./dx) .^2.0 .* phi[:,:,t-1] * Ax) .-M.*(A .- A0).*abs_nabra_phi .+ (a.*V[:,:,t-1] .- b.*U[:,:,t-1]).*abs_nabra_phi)./tau .+ (k1p .+ k2p)./2.0
         phi[:,:,t] = phi[:,:,t-1] .+ dt.*del_phi
 
-        reaction_U = fu(U[:,:,t-1], V[:,:,t-1], phi[:,:,t-1]).-Xu.*U[:,:,t-1].*abs_nabra_phi.^2 ./(sum(dx.*dy.*abs_nabra_phi.^2))
+        reaction_U = (k1u.+k2u)./2.0.-Xu.*U[:,:,t-1].*abs_nabra_phi.^2 ./(sum(dx.*dy.*abs_nabra_phi.^2))
 
-        reaction_V = fv(U[:,:,t-1], V[:,:,t-1], phi[:,:,t-1]).-Xv.*V[:,:,t-1].*abs_nabra_phi.^2 ./(sum(dx.*dy.*abs_nabra_phi.^2))
+        reaction_V = (k1v.+k2v)./2.0.-Xv.*V[:,:,t-1].*abs_nabra_phi.^2 ./(sum(dx.*dy.*abs_nabra_phi.^2))
 
         U[:,:,t] = U[:,:,t-1].+ dt .*(Du.*(phi[:,:,t-1] .*((1.0 ./dy) .^2 .* Ay * U[:,:,t-1] .+ (1.0 ./dx) .^2 .* U[:,:,t-1] * Ax) .+ ((1.0 ./dx) .* phi[:,:,t-1] *Bx .+ (1.0 ./dy) .* By *phi[:,:,t-1]) .*((1.0 ./dx) .* U[:,:,t-1] *Bx .+ (1.0 ./dy) .* By *U[:,:,t-1])) .+ reaction_U .- del_phi)
 
